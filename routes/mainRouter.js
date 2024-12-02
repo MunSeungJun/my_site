c33db10f9bdd97e2dcbc0b5ec891619f0fe933e1
Seung Jun <sbs2469@naver.com>
2024-12-02 16:26:44 +0900

    메인 라우터 | about, contact 랜더링

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs"
 
+/**
+ * 
+ */
+
 router.get('/', (req, res) => {
     res.render ('index', {
         layout: mainLayout,
@@ -9,8 +13,26 @@ router.get('/', (req, res) => {
         header: 'Page Header'
     });
 })
-router.get('/signup', (req, res) => {
-    res.render('join')
+/**
+ * About : 사이트 소개
+ * GET /about
+ */
+router.get('/about', (req, res) => {
+    res.render('about', {
+        layout:mainLayout,
+        title: 'About Title',
+        header: 'About Header'
+    });
+})
+/**
+ * 
+ */
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+        layout:mainLayout,
+        title: 'Contact Title',
+        header: 'Contact Header'
+    });
 })
 
 
