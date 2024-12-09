7611ed03406864eac6e9a74910051e75aabd67d0
Seung Jun <sbs2469@naver.com>
2024-12-09 16:49:33 +0900

    로그인 및 유지에러 처리

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose")
+mongoose.set('debug', true);
 
 const connectDB = async() => {
     try{
-        const conn = mongoose.connect(process.env.MONGODB_URI)
+        const conn = mongoose.connect(process.env.MONGODB_URI, {
+            authSource: 'admin'
+        })
         console.log('---------db 연결 성공---------')
         if (!conn) {
             console.log('---------db 연결 실패---------')
