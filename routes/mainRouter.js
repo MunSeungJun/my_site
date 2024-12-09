06e03d9f9dc20d5c0985226a06f890604b57ceb4
Seung Jun <sbs2469@naver.com>
2024-12-09 17:02:36 +0900

    메인라우터 | 첫페이지, 최신글보기

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs"
+const Post = require("../models/Post.js")
 
 /**
- * 
+ * 첫 페이지
+ * GET /home
  */
-
-router.get('/', (req, res) => {
-    res.render ('index', {
+router.get('/', async (req, res) => {
+    const data = await Post.find()
+    res.render('index', {
+        data,
         layout: mainLayout,
         title: 'Page Title',
         header: 'Page Header'
     });
+})
+/**
+ * 최신글 보기
+ * GET /post
+ */
+router.get('/posts/:id', async (req, res) =>{
+    const data = await Post.findById(req.params.id)
+    res.status(200).send(data)
 })
 /**
  * About : 사이트 소개
@@ -19,7 +30,7 @@ router.get('/', (req, res) => {
  */
 router.get('/about', (req, res) => {
     res.render('about', {
-        layout:mainLayout,
+        layout: mainLayout,
         title: 'About Title',
         header: 'About Header'
     });
@@ -29,10 +40,26 @@ router.get('/about', (req, res) => {
  */
 router.get('/contact', (req, res) => {
     res.render('contact', {
-        layout:mainLayout,
+        layout: mainLayout,
         title: 'Contact Title',
         header: 'Contact Header'
     });
 })
 
 module.exports = router;
+
+// 임시 POST
+// Post.insertMany([
+//     {
+//         title:"의무교육...",
+//         content:"의무교육은 무상으로 한다. 대통령은 헌법과 법률이 정하는 바에 의하여 국군을 통수한다."
+//     },
+//     {
+//         title:"타인의 범죄행위...",
+//         content:"타인의 범죄행위로 인하여 생명·신체에 대한 피해를 받은 국민은 법률이 정하는 바에 의하여 국가로부터 구조를 받을 수 있다."
+//     },
+//     {
+//         title:"헌법재판소에서...",
+//         content:"헌법재판소에서 법률의 위헌결정, 탄핵의 결정, 정당해산의 결정 또는 헌법소원에 관한 인용결정을 할 때에는 재판관 6인 이상의 찬성이 있어야 한다."
+//     }
+// ])
